70f0ba18b7aed00c375529347cf3d77768a8c164
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2026-06-25 20:23:20 -0300

    add comment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    // Proxy para evitar problemas de CORS em dev
     proxy: {
       '/api': {
         target: 'http://localhost:3333',
